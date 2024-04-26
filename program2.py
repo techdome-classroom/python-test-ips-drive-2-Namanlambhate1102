18d40f36963052f5faaeccde46145fd760726d22
Namanlambhate1102 <168165532+Namanlambhate1102@users.noreply.github.com>
2024-04-26 16:04:10 +0530

    Update program2.py

--- program2.py
+++ program2.py
@@ -1,7 +1,23 @@
-class Solution(object):
-    def romanToInt(self, s):
-        """
-        :type s: str
-        :rtype: int
-        """
-        pass
+function longestSubstring(s) {
+    let mxlen = 0;
+    let left = 0;
+    const charMap = new Map();  
+
+    for (let right = 0; right < s.length; right++) {
+        const currChar = s[right];
+
+         
+        if (charMap.has(currChar) && charMap.get(currChar) >= left) {
+            left = charMap.get(currChar) + 1;
+        }
+
+         charMap.set(currChar, right);
+
+         mxlen = Math.max(mxlen, right - left + 1);
+    }
+
+    return mxlen;
+}
+
+
+module.exports = { longestSubstring };
